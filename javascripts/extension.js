524170f5ca0dc47418ce819fbb936dbac841ec55
Felipe Elias Philipp <felipeelias@gmail.com>
2011-07-31 15:50:30 +0200

    Adds a "Share to Google+" button

--- javascripts/extension.js
+++ javascripts/extension.js
@@ -1,1 +1,7 @@
-alert('hello world')
+$("div.entry").live('click', function( e ) {
+  var item = $(e.target).closest('.entry');
+
+  if (!item.find('.entry-actions span.google-plus').length) {
+    item.find(".entry-actions span.tag").after($('<span class="link google-plus">Share to Google+</span>'));
+  }
+});
