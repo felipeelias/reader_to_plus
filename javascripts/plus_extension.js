3fd0d15a706f82404276191ecb12da92f891d923
Felipe Elias Philipp <felipeelias@gmail.com>
2011-07-31 22:17:23 +0200

    Refactor in plus_extension.js

--- javascripts/plus_extension.js
+++ javascripts/plus_extension.js
@@ -1,28 +1,30 @@
-function getParams() {
-  var raw_params = document.URL.replace(document.baseURI + '?', ''),
-      params = {};
+(function($) {
+  var params = (function() {
+    var url_tokens = document.URL.replace(document.baseURI.concat('?'), '').split(/&/g),
+        params = {};
 
-  $.each(raw_params.split(/&/g), function() {
-    var slice = this.split(/=/);
-    params[slice[0]] = slice[1];
-  });
-
-  return params;
-}
-
-$(function() {
-  var params = getParams();
-
-  $(".n-Nd").trigger('click');
-
-  var reloader = function() {
-    var div = $("div.n-Ob div[contenteditable]");
-    if ( !div.length ) {
-      setTimeout(reloader, 200);
-    } else {
-      div.text(params['status']);
+    for(piece in url_tokens) {
+      var slice = url_tokens[piece].split(/=/);
+      params[slice[0]] = decodeURIComponent(slice[1]);
     }
-  };
 
-  setTimeout(reloader, 200);
-});
+    return params;
+  })();
+
+  $(function() {
+    $(".n-Nd").trigger('click');
+
+    var reloader = function() {
+      var div = $("div.n-Ob div[contenteditable]");
+      if ( !div.length ) {
+        setTimeout(reloader, 200);
+      } else {
+        var text = params['title'].replace(/\+/g, ' ') + ': ' + params['href'];
+        div.text(text);
+      }
+    };
+
+    setTimeout(reloader, 200);
+  });
+})(jQuery);
+
